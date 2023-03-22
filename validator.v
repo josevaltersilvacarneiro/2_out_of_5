5c86c6fa05fdc899a6b14e26e7205c6a884e0b0f
José Valter Silva Carneiro <git@josevaltersilvacarneiro.net>
2023-03-22 18:33:24 -0300

    A bug was solved: the validator module was wrong

--- validator.v
+++ validator.v
@@ -2,52 +2,38 @@ module validator (a, b, c, d, e, v);
 	input a, b, c, d, e;
 	output v;
 	
-	wire an, bn, dn, en;
-	wire n1, n2, n4, n6, n7, n8;
+	wire an, bn, cn, dn, en;
 	wire o1, o2, o3, o4, o5, o6, o7, o8, o9, o10;
 	
-	wire nr5;
-	
 	// 1
-	nand(n1, a, b, c);
-	and(o1, n1, d, e);
+	and(o1, an, bn, cn, d, e);
 	
 	// 2
-	nand(n2, a, b);
-	not(dn, d);
-	and(o2, n2, c, dn, e);
+	and(o2, an, bn, c, d, en);
 	
 	// 3
-	not(en, e);
-	and(o3, n2, c, d, en);
+	and(o3, an, bn, c, dn, e);
 	
 	// 4
-	nand(n4, c, d);
-	not(an, a);
-	and(o4, an, b, n4, e);
+	and(o4, an, b, cn, dn, e);
 	
 	// 5
-	nor(nr5, c, d, e); // De Morgan
-	and(o5, an, nr5);
+	and(o5, an, b, cn, d, en);
 	
 	// 6
-	nand(n6, d, e);
-	and(o6, an, b, c, n6);
+	and(o6, an, b, c, dn, en);
 	
 	// 7
-	nand(n7, b, c, d);
-	and(o7, a, n7, e);
+	and(o7, a, b, cn, dn, en);
 	
 	// 8
-	nand(n8, b, c);
-	or(o8, a, n8, d, en);
+	and(o8, a, bn, cn, dn, e);
 	
 	// 9
-	not(bn, b);
-	or(o9, a, bn, c, n6);
+	and(o9, a, bn, cn, d, en);
 	
 	// 10
-	and(o10, a, b, nr5);
+	and(o10, a, bn, c, dn, en);
 	
 	// end
 	or(v, o1, o2, o3, o4, o5, o6, o7, o8, o9, o10);
